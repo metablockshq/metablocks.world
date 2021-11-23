d1e8440ef19ac7be50f95481a741ff5eb52d88ac
Shivek Khurana <khuranashivek@outlook.com>
2021-11-23 14:55:55 +0530

    Add authors

--- content/authors/sarthak-khurana.md
+++ content/authors/sarthak-khurana.md
@@ -1,8 +1,24 @@
 ---
 slug: sarthakkhurana
-name: Sarthak Khurana
+name: Sarthak Khurana 🔮
 twitter: knighton_bloke
 profilePicture: /img/content/authors/ahhranjh_400x400.jpeg
 shortBio: Co-founder Meta Blocks
 ---
-Don't be lazy, write a Bio
+Hey there 🤙 I'm creating Meta Blocks along with my brother [Shivek](https://metablocks.world/authors/shivekkhurana).
+
+# What do I do at Meta Blocks?
+
+My responsibilities include leading marketing, partnerships and community growth & management. 
+
+# Background
+
+I've been working in the marketing space for almost 5 years with a focus on growth hacking. I started out my journey in the gaming media sector where I scaled and optimized media and content to reach millions of consumers. From there, I went on bootstrap my own start-up, Trigger Two Entertainment, where I partnered with AAA studios like Ubisoft, WB, CD Projekt Red, Rockstar and Sony. 
+
+In early 2021, I joined prePO, a DeFi protocol that lets users invest in Pre IPO companies like SpaceX. As the first marketing hire and a community and marketing specialist, I scaled the community and engagement by 10929%. 
+
+I'm 19 and I dropped out after high school to work full time in the blockchain industry. 
+
+# Contact me
+
+The best way to reach out to me is on Twitter. You can also drop a line at Meta Block's Twitter and Discord to reach me.
--- src/templates/Author.js
+++ src/templates/Author.js
@@ -1,48 +1,67 @@
-import React from 'react';
-import {useRouteData} from 'react-static';
-import convert from 'htmr';
+import React from "react";
+import { useRouteData } from "react-static";
+import convert from "htmr";
 
-import Shell from '../components/Shell';
-import SEO from '../components/SEO';
-import Markdown from '../components/Markdown';
-import colors from '../utils/colors';
+import Shell from "../components/Shell";
+import SEO from "../components/SEO";
+import Markdown from "../components/Markdown";
+import colors from "../utils/colors";
 
-const Li = ({href, title}) => <li><a className="blue" href={href}>{title}</a></li>
+const Li = ({ href, title }) => (
+  <li>
+    <a className="blue" href={href}>
+      {title}
+    </a>
+  </li>
+);
 
-const SocialLinks = ({twitter, youtube, github, medium}) => {
-  return (<ul>
-	    {twitter && <Li title={"Twitter"} href={`https://twitter.com/${twitter}`}/>}
-	    {github && <Li title={"Github"} href={`https://github.com/${github}`}/>}
-	    {youtube && <Li title={"YouTube"} href={youtube} />}
-	    {medium && <Li title={"Medium"} href={`https://${medium}.medium.com`}/>}
-	  </ul>)
-}
+const SocialLinks = ({ twitter, youtube, github, medium }) => {
+  return (
+    <ul>
+      {twitter && (
+        <Li title={"Twitter"} href={`https://twitter.com/${twitter}`} />
+      )}
+      {github && <Li title={"Github"} href={`https://github.com/${github}`} />}
+      {youtube && <Li title={"YouTube"} href={youtube} />}
+      {medium && <Li title={"Medium"} href={`https://${medium}.medium.com`} />}
+    </ul>
+  );
+};
 
 const Author = () => {
-  const {contents, name, profilePicture, shortBio,
-	 twitter, github, youtube, medium} = useRouteData();
+  const {
+    contents,
+    name,
+    profilePicture,
+    shortBio,
+    twitter,
+    github,
+    youtube,
+    medium,
+  } = useRouteData();
 
-  return (<React.Fragment>
-	    <SEO title={`Krim / ${name}`} />
-	    <Shell>
-	      <div className="w-90 w-70-m w-50-l center flex items-center">
-		<div>
-		  <img src={profilePicture} alt={name} className="br-100" />
-		</div>
-		<div className="pl3">
-		  <div className="f1 b">
-		    {name}
-		  </div>
-		  <SocialLinks twitter={twitter} github={github}
-			       medium={medium} youtube={youtube}
-		  />
-		</div>
-	      </div>
-	      <div className="mt2">
-		<Markdown contents={contents} />
-	      </div>
-	    </Shell>
-	  </React.Fragment>);
+  return (
+    <React.Fragment>
+      <SEO title={`Krim / ${name}`} />
+      <Shell>
+        <div className="w-90 w-70-m w-50-l center flex items-center">
+          <img src={profilePicture} alt={name} className="w-30 br-100" />
+          <div className="pl3">
+            <div className="f4 f3-m f2-l b">{name}</div>
+            <SocialLinks
+              twitter={twitter}
+              github={github}
+              medium={medium}
+              youtube={youtube}
+            />
+          </div>
+        </div>
+        <div className="mt2">
+          <Markdown contents={contents} />
+        </div>
+      </Shell>
+    </React.Fragment>
+  );
 };
 
 export default Author;
