83f8d099041cc5cec883ca53bb6e6f5e9d24836e
Shivek Khurana <khuranashivek@outlook.com>
2022-01-28 21:51:39 +0530

    Fix feed

--- node.api.js
+++ node.api.js
@@ -1,53 +1,54 @@
-import {promises as fs} from "fs"
+import { promises as fs } from "fs";
 
-import R from "ramda"
-import {Feed} from "feed";
+import R from "ramda";
+import { Feed } from "feed";
 
-const feedFactory = () => new Feed({
-  title: "Krim Labs Blog Feed",
-  description: "Articles about Clojure, Blockchain, Ethereum, Privacy, UX and SaaS",
-  id: "https://krimlabs.com",
-  link: "https://krimlabs.com",
-  language: "en",
-  favicon: "https://krimlabs.com/favicon.ico",
-  copyright: "All rights reserved 2017, Krim Labs",
-  generator: "https://github.com/jpmonette/feed"
-})
+const feedFactory = () =>
+  new Feed({
+    title: "Meta Blocks - Blog Feed",
+    description:
+      "Meta Blocks is an NFT Evolution Protocol that lets you build up your NFTs, buy accessories that you like and trade them on secondary marketplaces.",
+    id: "https://metablocks.world",
+    link: "https://metablocks.world",
+    language: "en",
+    favicon: "https://metablocks.world/favicon.ico",
+    copyright: "All rights reserved 2021, Meta Blocks",
+    generator: "https://github.com/jpmonette/feed",
+  });
 
-
-const addPostToFeed = feed => post => feed.addItem({
-  title: post.data.title,
-  id: `https://krimlabs.com/blog/${post.data.slug}`,
-  link: `https://krimlabs.com/blog/${post.data.slug}`,
-  description: post.data.subTitle || "",
-  content: post.data.contents,
-  date: post.data.publishedOn,
-  image: `https://krimlabs.com${post.data.heroImg}`,
-  author: {
-    name: post.data.author.name,
-    link: `https://krimlabs.com/authors/${post.data.author.slug}`
-  }
-})
-
+const addPostToFeed = (feed) => (post) =>
+  feed.addItem({
+    title: post.data.title,
+    id: `https://metablocks.world/blog/${post.data.slug}`,
+    link: `https://metablocks.world/blog/${post.data.slug}`,
+    description: post.data.subTitle || "",
+    content: post.data.contents,
+    date: post.data.publishedOn,
+    image: `https://metablocks.world${post.data.heroImg}`,
+    author: {
+      name: post.data.author.name,
+      link: `https://metablocks.world/authors/${post.data.author.slug}`,
+    },
+  });
 
 const writeFeeds = async (distPath, feed) => {
-  await fs.writeFile(distPath + '/rss.xml', feed.rss2())
-  await fs.writeFile(distPath + '/feed.json', feed.json1())
-}
+  await fs.writeFile(distPath + "/rss.xml", feed.rss2());
+  await fs.writeFile(distPath + "/feed.json", feed.json1());
+};
 
 const pluginOptions = () => ({
-  afterBundle: async state => {
-    const routes = state.routes
-    const blogPosts = R.filter(r => R.startsWith("blog/", r.path), routes)
-    const feed = feedFactory()
-    const addPostToFeedFn = addPostToFeed(feed)
-    R.forEach(addPostToFeedFn, blogPosts)
+  afterBundle: async (state) => {
+    const routes = state.routes;
+    const blogPosts = R.filter((r) => R.startsWith("blog/", r.path), routes);
+    const feed = feedFactory();
+    const addPostToFeedFn = addPostToFeed(feed);
+    R.forEach(addPostToFeedFn, blogPosts);
 
-    const distPath = state.config.paths.DIST
-    await writeFeeds(distPath, feed)
-    console.log("Feed files generated.")
-    return state
+    const distPath = state.config.paths.DIST;
+    await writeFeeds(distPath, feed);
+    console.log("Feed files generated.");
+    return state;
   },
-})
+});
 
-export default pluginOptions
+export default pluginOptions;
